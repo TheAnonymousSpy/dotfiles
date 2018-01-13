9b19e66f998ed6bda855b110b5b71f6aed2d0f24
TheAnonymousSpy <zeb@trilobiet.nl>
2018-01-13 17:54:49 +0100

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down 
+while pgrep -x polybar > 0; do sleep 1; done
+
+# sleep 100
+
+# Launch bar1 and bar2
+polybar top  && polybar bottom
+
+
+
+
